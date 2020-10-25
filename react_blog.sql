7f6969cd49c8416cf7df0aea29ff55704d253bbd
QiuYiLiang <49298714+QiuYiLiang@users.noreply.github.com>
2020-10-25 16:07:05 +0800

    数据库

--- react_blog.sql
+++ react_blog.sql
@@ -3,15 +3,15 @@
 
  Source Server         : mysql
  Source Server Type    : MySQL
- Source Server Version : 80018
+ Source Server Version : 80021
  Source Host           : localhost:3306
  Source Schema         : react_blog
 
  Target Server Type    : MySQL
- Target Server Version : 80018
+ Target Server Version : 80021
  File Encoding         : 65001
 
- Date: 08/06/2020 17:50:01
+ Date: 25/10/2020 16:05:33
 */
 
 SET NAMES utf8mb4;
@@ -22,61 +22,48 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `admin_user`;
 CREATE TABLE `admin_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of admin_user
--- ----------------------------
-BEGIN;
-INSERT INTO `admin_user` VALUES (1, 'admin', 'admin');
-COMMIT;
+  `id` int DEFAULT NULL,
+  `userName` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_bin;
 
 -- ----------------------------
 -- Table structure for article
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type_id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `article_content` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `introduce` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `addTime` int(11) DEFAULT NULL,
-  `view_count` int(11) NOT NULL DEFAULT '0',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type_id` int DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  `article_content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  `introduce` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  `addTime` int DEFAULT NULL,
+  `view_count` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_bin;
 
 -- ----------------------------
--- Records of article
+-- Table structure for blog_content
 -- ----------------------------
-BEGIN;
-INSERT INTO `article` VALUES (1, 1, 'asda', 'asd', 'sadsad', 1583424000, 1231);
-INSERT INTO `article` VALUES (3, 3, 'niaho', '123', '2134', 1583424000, 123123);
-INSERT INTO `article` VALUES (8, 3, '你好', '你好啊', '你好呀', 1588780800, 0);
-COMMIT;
+DROP TABLE IF EXISTS `blog_content`;
+CREATE TABLE `blog_content` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  `type` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  `introduce` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  `content` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_bin;
 
 -- ----------------------------
 -- Table structure for type
 -- ----------------------------
 DROP TABLE IF EXISTS `type`;
 CREATE TABLE `type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `typeName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `orderNum` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `typeName` varchar(255) COLLATE utf8mb4_0900_bin DEFAULT NULL,
+  `orderNum` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of type
--- ----------------------------
-BEGIN;
-INSERT INTO `type` VALUES (1, '家', 1);
-INSERT INTO `type` VALUES (2, '视频', 2);
-INSERT INTO `type` VALUES (3, '生活', 3);
-COMMIT;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_bin;
 
 SET FOREIGN_KEY_CHECKS = 1;
